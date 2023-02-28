46e5c8b65567f0eb397520f6a1d41fd1cd4364ac
Sebastien Patez <sepatez@shi-r8-p5.s19.be>
2023-02-28 11:23:33 +0100

    did ex06

---
+++ C11WITHOUTREPO/ex06/ft_sort_string_tab.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_string_tab.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sepatez <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/28 11:03:54 by sepatez           #+#    #+#             */
+/*   Updated: 2023/02/28 11:03:56 by sepatez          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2)
+	{
+		if (*s1 != *s2)
+		{
+			return (*s1 - *s2);
+		}
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
+}
+
+void	ft_sort_string_tab(char **tab)
+{
+	int		i;
+	int		j;
+	char	*temp;
+
+	i = 0;
+	j = 1;
+	while (tab[i])
+	{
+		while (tab[j])
+		{
+			if (ft_strcmp(tab[i], tab[j]) > 0)
+			{
+				temp = tab[i];
+				tab[i] = tab[j];
+				tab[j] = temp;
+			}
+			j++;
+		}
+		i++;
+		j = i + 1;
+	}
+}
