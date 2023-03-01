b00f92b94152c0baaa03b34f22124ce869c3a9e6
Sebastien Patez <sepatez@shi-r8-p5.s19.be>
2023-03-01 06:45:03 +0100

    finished ex01 without testing yolo

---
+++ C12WITHOUTREPO/ex01/ft_list_push_front.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list_push_front.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sepatez <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/01 06:31:05 by sepatez           #+#    #+#             */
+/*   Updated: 2023/03/01 06:31:07 by sepatez          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_list.h"
+
+t_list	*ft_create_elem(void *data)
+{
+	t_list	*result;
+
+	result = malloc(sizeof(t_list));
+	if (!result)
+		return (0x0);
+	result->data = data;
+	result->next = 0x0;
+	return (result);
+}
+
+void	ft_list_push_front(t_list **begin_list, void *data)
+{
+	t_list	*new;
+
+	new = ft_create_elem(data);
+	new->next = *begin_list;
+	*begin_list = new;
+}
